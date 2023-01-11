a5762880548d39157db472fb5feecf9006c3ecd3
Marcktm <marck_tm@outlook.com>
2023-01-11 01:27:30 -0300

    Agregar archivos de proyecto.

---
+++ ProductoVendido.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal_ReyerosMarcos_CSharp
+{
+    internal class ProductoVendido
+    {
+        //Id, IdProducto, Stock, IdVenta 
+        private int Id;
+        private int idProducto;
+        private int stock;
+        private int idVenta;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ProyectoFinal_ReyerosMarcos_CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class Usuario
+    {
+        private int id;
+        private string nombre;
+        private string apellido;
+        private string nombreUsuiario;
+        private string contraseña;
+        private string mail;
+
+
+    }
+}
