adee03c687ff1c55626c008b33f81c670ba57e94
Marcktm <104657012+Marcktm@users.noreply.github.com>
2023-05-12 13:33:23 -0300

    Add files via upload

---
+++ ProyectoFinal-ReyerosMarcos-CSharp-master/Producto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal_ReyerosMarcos_CSharp
+{
+    internal class Producto
+    {
+        //Id, Descripcion, Costo, PrecioVenta, Stock, IdUsuario
+        private int Id;
+        private string descripcion;
+        private int costo;
+        private double precioVenta;
+        private int stock;
+        private int idUsuario;
+    }
+}
---
+++ ProyectoFinal-ReyerosMarcos-CSharp-master/Venta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal_ReyerosMarcos_CSharp
+{
+    internal class Venta
+    {
+        //Id, Comentarios, IdUsuario
+        private int id;
+        private string comentarios;
+        private int idUsuario;
+    }
+}
